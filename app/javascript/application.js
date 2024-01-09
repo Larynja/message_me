b30ab8828e0778709f2cb966fa4cb48429ceede3
Larynja <ddombrowski.py@gmail.com>
2024-01-09 19:26:19 +0100

    added navbar and icon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as jquery from "jquery"
 import "semantic-ui" 
+import jquery from "jquery";
+window.jQuery = jquery
+window.$ = jquery
+
+$(document).on('turbo:load', function() {
+    console.log('loaded turbo links')
+    $('.ui.dropdown').dropdown()
+});
