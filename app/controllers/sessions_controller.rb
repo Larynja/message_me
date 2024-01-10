5ba27965e0681cf07057c8a93a824756ca402fbe
Larynja <ddombrowski.py@gmail.com>
2024-01-10 22:43:57 +0100

    added user & message models

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(username: params[:session] [:username])
+    if user && user.authenticate(params[:session] [:password])
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as #{user.username}"   
+      redirect_to root_path
+    else
+      flash.now[:error] = "Login details incorrect"
+      render :new, status: :unprocessable_entity
+      end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have been logged out"
+    redirect_to login_path
+  end
+
 end
